bfc80a89a804d8450c3314d91178b9af91664ca7
patawatt <p.wattage@gmail.com>
2018-11-26 18:04:53 -0500

    Prep 11 3 way mergesort complete

--- preps/prep11/prep11_og.py
+++ preps/prep11/prep11_og.py
@@ -118,8 +118,8 @@ def mergesort3(lst: List) -> List:
         - the main helper merge3 takes in *three* sorted lists, and returns
           a sorted list that contains elements from all of its inputs.
 
-    HINT: depending on your implementation, you might need another base case
-            when len(lst) == 2 to avoid an infinite recursion error.
+    HINT: depending on your impementation, you might need another base case when
+          len(lst) == 2 to avoid an infinite recursion error.
 
     >>> mergesort3([10, 2, 5, -6, 17, 10])
     [-6, 2, 5, 10, 10, 17]
@@ -127,15 +127,7 @@ def mergesort3(lst: List) -> List:
     if len(lst) < 2:
         return lst[:]
     else:
-        # Divide the list into three parts, and sort them recursively.
-        mid1 = len(lst) // 3
-        mid2 = 2*len(lst) // 3
-        a_sorted = mergesort3(lst[:mid1])
-        b_sorted = mergesort3(lst[mid1:mid2])
-        c_sorted = mergesort3(lst[mid2:])
-
-        # Merge the two sorted halves. Need a helper here!
-        return merge3(a_sorted, b_sorted, c_sorted)
+        pass
 
 
 # TODO: Implement this function!
@@ -153,56 +145,10 @@ def merge3(lst1: List, lst2: List, lst3: List) -> List:
     up your code into one or more helpers to divide up (and test!) each part
     separately.
     """
-    index1 = 0
-    index2 = 0
-    index3 = 0
-    merged = []
-    while index1 < len(lst1) and index2 < len(lst2) and index3 < len(lst3):
-        if lst1[index1] <= lst2[index2]:
-            if lst1[index1] <= lst3[index3]:
-                merged.append(lst1[index1])
-                index1 += 1
-            else:
-                merged.append(lst3[index3])
-                index3 += 1
-        else:
-            if lst2[index2] <= lst3[index3]:
-                merged.append(lst2[index2])
-                index2 += 1
-            else:
-                merged.append(lst3[index3])
-                index3 += 1
+    pass
 
-    # lst1 and lst2 have remaining values.
-    while index1 < len(lst1) and index2 < len(lst2):
-        if lst1[index1] <= lst2[index2]:
-            merged.append(lst1[index1])
-            index1 += 1
-        else:
-            merged.append(lst2[index2])
-            index2 += 1
-    # lst2 and lst3 have remaining values.
-    while index2 < len(lst2) and index3 < len(lst3):
-        if lst2[index2] <= lst3[index3]:
-            merged.append(lst2[index2])
-            index2 += 1
-        else:
-            merged.append(lst3[index3])
-            index3 += 1
-    # lst3 and lst1 have remaining values.
-    while index3 < len(lst3) and index1 < len(lst1):
-        if lst3[index3] <= lst1[index1]:
-            merged.append(lst3[index3])
-            index3 += 1
-        else:
-            merged.append(lst1[index1])
-            index1 += 1
 
-    # The remaining elements of the other lists
-    # can all be added to the end of <merged>.
-    return merged + lst1[index1:] + lst2[index2:] + lst3[index3:]
-
-    # TODO: Implement this function
+# TODO: Implement this function
 def kth_smallest(lst: List, k: int) -> Any:
     """Return the <k>-th smallest element in <lst>.
 
@@ -210,12 +156,11 @@ def kth_smallest(lst: List, k: int) -> Any:
     Note: for convenience, k starts at 0, so kth_smallest(lst, 0) == min(lst).
 
     Precondition: <lst> does not contain duplicates.
-    # >>> kth_smallest([10, 20, 30, 40], 2)
-    # 30
-    # >>> kth_smallest([10, 20, -4, 3], 0)
-    # -4
-    # >>> kth_smallest([10, 20, -4, 3], 2)
-    # 10
+
+    >>> kth_smallest([10, 20, -4, 3], 0)
+    -4
+    >>> kth_smallest([10, 20, -4, 3], 2)
+    10
     """
     # You may *not* sort the list here (this is easy but not very efficient).
     # Instead, use the following approach, based on quicksort:
@@ -224,42 +169,12 @@ def kth_smallest(lst: List, k: int) -> Any:
     #   2. Compare len(smaller) against k, and use the result to decide which
     #      list to recurse on (if any). As in your BST prep, you should only
     #      make one recursive call into either <smaller> or <bigger>, not both!
-
-    if k < 0 or k >= len(lst):
-        raise IndexError
-    elif len(lst) < 2:
-        return lst[0]
-    else:
-        pivot = lst[0]
-        smaller = []
-        bigger = []
-
-        for item in lst[1:]:
-            if item <= pivot:
-                smaller.append(item)
-            else:
-                bigger.append(item)
-
-        if len(smaller) == k:
-            answer = pivot
-        elif len(smaller) > k:
-            answer = kth_smallest(smaller, k)
-        else:
-            answer = kth_smallest(lst[1:], k - 1)
-
-        # smaller.append(pivot)
-        #
-        # while len(smaller) != k:
-        #     if len(smaller) > k:
-        #         answer = kth_smallest(smallest)
-        #     elif len(smaller) < k:
-        #         answer = kth_smallest(biggest)
-        return answer
+    pass
 
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
-    # import python_ta
-    # python_ta.check_all()
+    import python_ta
+    python_ta.check_all()
