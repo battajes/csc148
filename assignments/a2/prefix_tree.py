16aca1f2870899ca464cb87557d42a4ee500f8ce
patawatt <36645994+patawatt@users.noreply.github.com>
2018-11-28 22:02:59 -0500

    Add files via upload
    Compressed tree initiated

--- assignments/a2/prefix_tree.py
+++ assignments/a2/prefix_tree.py
@@ -332,7 +332,7 @@ class SimplePrefixTree(Autocompleter):
         last_tree.weight = weight
         self.subtrees[i].subtrees.insert(j, last_tree)
 
-    def agg_weight(self, weight: float) -> None:
+    def agg_weight(self, weight: float, new_leaves: int = 1) -> None:
         """ Aggregate new weight with self.weight.
             Sum or average, depending on weight type of self.
         """
@@ -340,8 +340,9 @@ class SimplePrefixTree(Autocompleter):
             self.weight += weight
         elif self._weight_type == 'average':
             self.weight = (self.weight * self.num_leaves + weight)/ \
-                          (self.num_leaves + 1)
-        self.num_leaves += 1
+                          (self.num_leaves + new_leaves)
+        # this does not work when adding weight to a pre-existing leaf.
+        self.num_leaves += new_leaves
 
     def move_left(self, i: int) -> int:
         """
@@ -366,13 +367,16 @@ class SimplePrefixTree(Autocompleter):
                 i -= 1
         return i
 
-    def add_on(self, value: Any, w: float, prefix: List, c: int = 1) -> None:
+    def add_on(self, value: Any, w: float, prefix: List, c: int = 1) -> bool:
         """ Adds new internal nodes to the SimplePrefixTree assuming the tree
         already has values stored within it.  If the node to be inserted already
         exists, the existing node's weight is updated instead. When a new node
         needs to be created, self.add() is called.
-        """
 
+        Returns True if the item added on is already a leaf.  Returns False if
+        the leaf was not inserted yet.
+        """
+        past_leaf = False
         if c <= len(prefix):
 
             # if prefix[:c] in self.subtrees:
@@ -383,9 +387,13 @@ class SimplePrefixTree(Autocompleter):
                 i = -1
 
             if i >= 0:
-                if c == 1:
-                    # add weight to root since new prefix matches deeper prefix.
-                    self.agg_weight(w)
+                # if c == 1:
+                #     # add weight to root since new prefix matches deeper prefix.
+                #     # self.agg_weight(w)
+                #     if past_leaf:
+                #         self.weight += w
+                #     else:
+                #         self.agg_weight(w)
 
                 if c == len(prefix):
                     # this case is not used if tree contains ('ab', ['a', 'b']
@@ -400,18 +408,52 @@ class SimplePrefixTree(Autocompleter):
 
                     if j < 0:
                         self.add_leaf(value, w, i, 0)
+                        # new addition
+                        self.subtrees[i].agg_weight(w)
+                        i = self.move_left(i)
+                        return False
                     else:
-                        self.subtrees[i].subtrees[j].agg_weight(w)
+                        # this is the case if a leaf is being reinforced.
+                        self.subtrees[i].subtrees[j].weight += w
+                        # new addition
+                        self.subtrees[i].weight += w
+                        i = self.move_left(i)
+                        return True
 
-                    self.subtrees[i].agg_weight(w)
+
+                    # just realized this gives all the trees 2 leaves when
+                    # a leaf is inserted the second time.
+                    # could keep track of imaginary leaves, and only use
+                    # to calc weight of leaves
+                    # could make a separate agg_weight function for leaves.
+                    # self.subtrees[i].agg_weight(w)
 
                 else:
                     # add weight to subtree since its prefix matches new prefix.
-                    self.subtrees[i].agg_weight(w)
-                    i = self.move_left(i)
-                    self.subtrees[i].add_on(value, w, prefix, c + 1)
+                    # self.subtrees[i].agg_weight(w)
+                    # i = self.move_left(i)
+                    # self.subtrees[i].add_on(value, w, prefix, c + 1)
+                    past_leaf = self.subtrees[i].add_on(value, w, prefix, c + 1)
+
+                    if past_leaf:
+                        self.subtrees[i].weight += w
+                    else:
+                        self.subtrees[i].agg_weight(w)
+                        i = self.move_left(i)
+
+                    if c == 1:
+                        # add weight to root since new prefix matches deeper prefix.
+                        # self.agg_weight(w)
+                        if past_leaf:
+                            self.agg_weight(w, 0)
+                        else:
+                            self.agg_weight(w)
+
             else:
                 self.add(value, w, prefix, c)
+        # new addition
+        return past_leaf
+
 
     def autocomplete(self, prefix: List,
                      limit: Optional[int] = None) -> List[Tuple[Any, float]]:
@@ -546,7 +588,7 @@ class SimplePrefixTree(Autocompleter):
 ################################################################################
 # CompressedPrefixTree (Task 6)
 ################################################################################
-class CompressedPrefixTree(Autocompleter):
+class CompressedPrefixTree(SimplePrefixTree):
     """A compressed prefix tree implementation.
 
     While this class has the same public interface as SimplePrefixTree,
@@ -593,35 +635,107 @@ class CompressedPrefixTree(Autocompleter):
     value: Optional[Any]
     weight: float
     subtrees: List[CompressedPrefixTree]
+    _weight_type: str
 
-    def __len__(self) -> int:
-        """Return the number of values stored in this Autocompleter.
+    # def __len__(self) -> int:
+    #     """Return the number of values stored in this Autocompleter.
+    #
+    #     === Representation Invariants ===
+    #     - (EMPTY TREE):
+    #     If self.weight == 0, then self.value == [] and self.subtrees == [].
+    #     This represents an empty simple prefix tree.
+    #
+    #     """
+    #     raise NotImplementedError
+    #
+    # def insert(self, value: Any, weight: float, prefix: List) -> None:
+    #     """Insert the given value into this Autocompleter.
+    #
+    #     The value is inserted with the given weight, and is associated with
+    #     the prefix sequence <prefix>.
+    #
+    #     If the value has already been inserted into this prefix tree
+    #     (compare values using ==), then the given weight should be *added* to
+    #     the existing weight of this value.
+    #
+    #     Preconditions:
+    #         weight > 0
+    #         The given value is either:
+    #             1) not in this Autocompleter
+    #             2) was previously inserted with the SAME prefix sequence
+    #     """
+    #     raise NotImplementedError
+
+    def add_leaf(self, value: Any, weight: float, i: int, j: int):
+        """ Adds final non-prefix value to the SimplePrefixTree as a leaf.
+        """
+
+        last_tree = CompressedPrefixTree(self._weight_type)
+        last_tree.value = value
+        last_tree.weight = weight
+        self.subtrees[i].subtrees.insert(j, last_tree)
+
+    def add_on(self, value: Any, w: float, prefix: List, c: int = 1):
+
+        print("This will add new leaves to a full tree")
+
+    def add(self, value: Any, weight: float, prefix: List, c: int = 1) -> None:
+        """ Adds new internal nodes to a SimplePrefixTree that is empty, or
+        that does not already contain these nodes.
 
         === Representation Invariants ===
-        - (EMPTY TREE):
-        If self.weight == 0, then self.value == [] and self.subtrees == [].
-        This represents an empty simple prefix tree.
+        - the nodes to be created do not exist in the SimplePrefixTree
 
         """
-        raise NotImplementedError
+        new_tree = CompressedPrefixTree(self._weight_type)
+        new_tree.value = prefix
+        new_tree.weight = weight
+        new_tree.num_leaves += 1
 
-    def insert(self, value: Any, weight: float, prefix: List) -> None:
-        """Insert the given value into this Autocompleter.
+        self.subtrees.append(new_tree)
+        self.add_leaf(value, weight, -1, -1)
+        self.agg_weight(weight)
+        self.subtrees[0].agg_weight(weight)
 
-        The value is inserted with the given weight, and is associated with
-        the prefix sequence <prefix>.
 
-        If the value has already been inserted into this prefix tree
-        (compare values using ==), then the given weight should be *added* to
-        the existing weight of this value.
-
-        Preconditions:
-            weight > 0
-            The given value is either:
-                1) not in this Autocompleter
-                2) was previously inserted with the SAME prefix sequence
-        """
-        raise NotImplementedError
+        # if c <= len(prefix):
+        #     new_tree = SimplePrefixTree(self._weight_type)
+        #     new_tree.value = prefix[:c]
+        #     new_tree.weight = weight
+        #     # new attribute needs testing
+        #     new_tree.num_leaves += 1
+        #
+        #     # need to find proper location here relative to values in list.
+        #     # if list is empty, append.
+        #     # if not insert appropriately.
+        #     if self.subtrees == []:
+        #         i = -1
+        #         self.subtrees.append(new_tree)
+        #         self.subtrees[i].add(value, weight, prefix, c + 1)
+        #     else:
+        #         i = self.find_place(weight)
+        #         self.subtrees.insert(i, new_tree)
+        #         self.subtrees[i].add(value, weight, prefix, c + 1)
+        #
+        #     if c == 1:
+        #         # didn't assign these on the way in, so do it on the way out.
+        #         # this line was not giving the songbook tree a sum weight of 25
+        #         # self.weight = weight
+        #         self.agg_weight(weight)
+        #         # this was already included in agg_weight, added one too many.
+        #         # self.num_leaves += 1
+        #     if c == len(prefix):
+        #         # i value found from find_place().
+        #         self.add_leaf(value, weight, i, 0)
+        #
+        # elif len(prefix) == 0:
+        #     # change add_leaf to work here. It goes too deep.
+        #     last_tree = SimplePrefixTree(self._weight_type)
+        #     last_tree.value = value
+        #     last_tree.weight = weight
+        #     self.subtrees.append(last_tree)
+        #     self.weight = weight
+        #     self.num_leaves += 1
 
 def binary_search(l: list, v: Any) -> int:
     """ Return the index of the first occurrence of v in L, or return -1
@@ -666,14 +780,14 @@ def take_weight(elem: Tuple[Any, int]) -> int:
 def at_lim(limit: Optional[int], found: int) -> bool:
 
     if limit is None:
-        at_lim = False
+        lim = False
     else:
         if found >= limit:
-            at_lim = True
+            lim = True
         else:
-            at_lim = False
+            lim = False
 
-    return at_lim
+    return lim
 
 
 if __name__ == '__main__':
@@ -682,17 +796,18 @@ if __name__ == '__main__':
     # python_ta.check_all(config={
     #     'max-nested-blocks': 4
     # })
-    x = SimplePrefixTree('average')
-    x.insert('car', 20, ['c', 'a', 'r'])
-    x.insert('cat', 10, ['c', 'a', 't'])
-    x.insert('care', 5, ['c', 'a', 'r', 'e'])
-    print(str(x))
-    # y = x.autocomplete([])
-    # print(y)
-    # 1, 2, 4
-    x.remove(['c', 'a', 'r', 'e'])
-    # x.remove(['c', 'a', 'r'])
-    # x.remove(['c', 'a', 't'])
-    # x.remove(['c', 'a'])
-    print(str(x))
-
+    # x = SimplePrefixTree('average')
+    # x.insert('car', 20, ['c', 'a', 'r'])
+    # x.insert('cat', 10, ['c', 'a', 't'])
+    # x.insert('care', 5, ['c', 'a', 'r', 'e'])
+    # print(str(x))
+    # # y = x.autocomplete([])
+    # # print(y)
+    # # 1, 2, 4
+    # x.remove(['c', 'a', 'r', 'e'])
+    # # x.remove(['c', 'a', 'r'])
+    # # x.remove(['c', 'a', 't'])
+    # # x.remove(['c', 'a'])
+    # print(str(x))
+    x = CompressedPrefixTree()
+    x.insert('hi', 1, ['h', 'i'])
